8eef28dc1adae9aa6279ed365a39161b2a8d3597
MohamedSamiAG <mohamed.sami.gawad@gmail.com>
2025-11-05 02:27:31 +0200

    Update: Added UI improvements and a clear-all button for better user experience.

--- Habit-Trackers.js
+++ Habit-Trackers.js
@@ -1,18 +1,22 @@
 const Cards = document.getElementById("bottom_habit");
 const nwe = document.getElementById("nwe");
+const AllHabits = document.getElementById("AllHabits");
 
-
+let id = 0;
+//  New Habit bottom_habit
 Cards.addEventListener("click", () => {
-nwe.innerHTML = `
+   
+    nwe.innerHTML = `
+    <div class="popups">
     <div class="popup">
-  <div class="popup-box">
+    <div class="popup-box">
     
     <div class="header">
-      <h2>Add Habit</h2>
-      <i id="closeIcon" class="fa-solid fa-xmark closeX"></i>
+    <h2>Add Habit</h2>
+    <i id="closeIcon" class="fa-solid fa-xmark closeX"></i>
     </div>
-   
-  <hr class="separator">
+    
+    <hr class="separator">
     <label for="habit">Name</label>
     <input type="text" id="habit" placeholder="e.g. Drink Water" class="sss">
 
@@ -23,23 +27,30 @@ nwe.innerHTML = `
     <button id="closeBtn">Cancel</button>
 </div>
   </div>
-</div>
+  </div>
+  </div>
   `;
-//save buttom
-const saveBtn = document.getElementById("saveBtn");
-
-saveBtn.addEventListener("click", () => {
-
+  
+  
+  
+  //save buttom
+  const saveBtn = document.getElementById("saveBtn");
+  const nooot = document.getElementById("nooot");
   const input1 = document.getElementsByClassName("sss")[0];
   const input2 = document.getElementsByClassName("sss")[1];
-
-  console.log("Habit saved:", input1.value, input2.value);
   
-//creat tr
-  const tr = document.createElement("tr");
-  //Add main habit name
-  tr.innerHTML = `<td style="padding: 0 5px;" ><i class="ri-bowl-line" style="padding: 0 4px;"></i>${input1.value}</td>
-    <td><i class="ri-close-circle-line"></i></td>
+  saveBtn.addEventListener("click", () => {
+  
+      id++
+      
+      console.log(`Habit saved:", ${input1.value}${input2.value} ${id}`);
+      
+      //creat tr
+      const tr = document.createElement("tr");
+      // ..............Weekly Board..........
+      tr.id = `habit-${id}`;
+      tr.innerHTML = `<td  style="padding: 0 5px;" ><div class="vbb"><i class="ri-bowl-line" style="padding: 0 4px;"></i>${input1.value}${id}</di></td>
+      <td><i class="ri-close-circle-line"></i></td>
     <td><i class="ri-verified-badge-line"></i></td>
     <td><i class="ri-verified-badge-line"></i></td>
     <td><i class="ri-close-circle-line"></i></td>
@@ -48,33 +59,53 @@ saveBtn.addEventListener("click", () => {
     <td><i class="ri-verified-badge-line"></i></td>`;
 
 //Add tr in table
+
+
 habitTable.appendChild(tr);
 
-//Add todayHabits
- AllHabits.innerHTML =`<div id="checkPart">
-                    <div class="toggle_button">
-                        <label class="switch" >
-                            <input type="checkbox">
-                            <span class="slider round"></span>
+
+//....Add todayHabits.....
+const habitDiv = document.createElement("div");
+habitDiv.id = `habit-${id}`;
+habitDiv.innerHTML = `<div id="checkPart">
+   <div class="toggle_button">
+<label class="switch" >
+<input type="checkbox">
+<span class="slider round"></span>
                         </label>
-
+                        
                     </div>
-                     <i class="ri-bowl-line" style="padding-left: 9px;"></i> 
+                    <i class="ri-bowl-line" style="padding-left: 9px;"></i> 
                     
-                    <h4>${input1.value}</h4> 
+                    <h4 class = "asdasd">${input1.value}${id}</h4> 
+                    <div class="RegPart">
+                    <button class="RegButton"><i class="ri-file-list-2-line" ></i></button>  <button class="RegButton"><i class="ri-pencil-line" ></i></button>  
+                    <button class="RegButton bin"><i class="ri-delete-bin-6-line" id="deleteSingle-${id}"></i>
+</button>   
+                    </div>
+                    </div>
+                    
+                
+                
+`;
 
-                </div>
+AllHabits.appendChild(habitDiv);
 
-                <div class="RegPart">
-                    <button class="RegButton"><i class="ri-file-list-2-line" ></i></button>  <button class="RegButton"><i class="ri-pencil-line" ></i></button>  <button class="RegButton bin"><i class="ri-delete-bin-6-line" ></i></button>   
-                </div>
-` ;
-
-
-  // inputs
-  input1.value = " ";
+                
+const deleteSingle = document.getElementById(`deleteSingle-${id}`);
+deleteSingle.addEventListener("click", function() {
+  habitDiv.remove();
+    tr.remove();  
+});
+                
+                // inputsى
+                input1.value = " ";
   input2.value = " ";
 });
+
+
+
+//....Add todayHabits.....
 //close icon
 const closeIcon = document.getElementById("closeIcon");
  closeIcon.addEventListener("click", () => {
@@ -85,8 +116,31 @@ const closeIcon = document.getElementById("closeIcon");
 
   // close buttom
   const closeBtn = document.getElementById("closeBtn");
-
+  
   closeBtn.addEventListener("click", () => {
-   nwe.innerHTML = "";   
-  });
+      nwe.innerHTML = "";   
+    });
 });
+//  ....................................New Habit bottom_habit......................................
+
+
+
+
+
+
+const clearTodays_Habits = document.getElementById("clearTodays_Habits");
+
+
+clearTodays_Habits.addEventListener("click",()=>{
+    AllHabits.innerHTML =""
+    habitTable.innerHTML=`<tr>
+    <th>Habit</th>
+    <th>Mon</th>
+    <th>Tue</th>
+    <th>Wed</th>
+    <th>Thu</th>
+    <th>Fri</th>
+    <th>Sat</th>
+    <th>Sun</th>
+  </tr> `
+})
